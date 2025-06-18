c4191c1af218d0dc42bc4a27cc23d3d4ff49ae57
Atif Kekec <kekecatf@outlook.com>
2025-06-18 12:38:30 +0300

    day9

---
+++ app/src/main/java/com/example/fitnessapp/data/projectfiles/NoteItem.kt
@@ -0,0 +1,7 @@
+package com.example.fitnessapp.data.projectfiles
+
+data class NoteItem(
+    val uid: String,
+    val text: String,
+    val isDone: Boolean
+)
--- app/src/main/java/com/example/fitnessapp/ui/notes/NotesScreen.kt
+++ app/src/main/java/com/example/fitnessapp/ui/notes/NotesScreen.kt
@@ -19,33 +19,22 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.fitnessapp.data.projectfiles.NoteItem
 import com.example.fitnessapp.ui.theme.FitnessAppTheme
 import com.example.fitnessapp.ui.theme.ThemeViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.launch
+import com.example.fitnessapp.data.projectfiles.NoteApiClient
+
 
 @Composable
 fun NotesScreen() {
     var todoText by remember { mutableStateOf("") }
     val todoItems = remember { mutableStateListOf<TodoItem>() }
     val user = FirebaseAuth.getInstance().currentUser
-    val firestore = FirebaseFirestore.getInstance()
+    val uid = user?.uid ?: ""
     var message by remember { mutableStateOf("") }
-
-    // Notları yükle
-    LaunchedEffect(Unit) {
-        user?.let {
-            firestore.collection("users").document(it.uid).get()
-                .addOnSuccessListener { doc ->
-                    val notes = doc.get("notes") as? List<Map<String, Any>>
-                    notes?.forEach {
-                        val text = it["text"] as? String ?: ""
-                        val isDone = it["isDone"] as? Boolean ?: false
-                        todoItems.add(TodoItem(text, isDone))
-                    }
-                }
-        }
-    }
 
     val themeViewModel: ThemeViewModel = viewModel(
         factory = ViewModelProvider.AndroidViewModelFactory(
@@ -54,10 +43,24 @@ fun NotesScreen() {
     )
     val isDarkTheme by themeViewModel.isDarkTheme.collectAsState()
 
+    val coroutineScope = rememberCoroutineScope()
 
-    // Tüm ekran temaya göre sarılıyor
+    // Notları yükle (PHP'den çek)
+    LaunchedEffect(uid) {
+        if (uid.isNotBlank()) {
+            try {
+                val notes = NoteApiClient.api.getNotes(uid)
+                todoItems.clear()
+                notes.forEach {
+                    todoItems.add(TodoItem(it.text, it.isDone))
+                }
+            } catch (e: Exception) {
+                message = "Notlar alınamadı: ${e.message}"
+            }
+        }
+    }
+
     FitnessAppTheme(darkTheme = isDarkTheme) {
-
         val backgroundColor = if (isDarkTheme) Color.Black else Color.White
 
         Column(
@@ -82,8 +85,25 @@ fun NotesScreen() {
                 Spacer(modifier = Modifier.width(8.dp))
                 Button(onClick = {
                     if (todoText.isNotBlank()) {
-                        todoItems.add(TodoItem(todoText, false))
+                        val newItem = TodoItem(todoText, false)
+                        todoItems.add(newItem)
                         todoText = ""
+
+                        // API’ye gönder
+                        coroutineScope.launch {
+                            try {
+                                val response = NoteApiClient.api.saveNote(
+                                    NoteItem(uid = uid, text = newItem.text, isDone = newItem.isDone)
+                                )
+                                if (response.isSuccessful) {
+                                    message = "Not kaydedildi"
+                                } else {
+                                    message = "Sunucu hatası: ${response.code()}"
+                                }
+                            } catch (e: Exception) {
+                                message = "Bağlantı hatası: ${e.message}"
+                            }
+                        }
                     }
                 }) {
                     Text("Ekle")
@@ -95,7 +115,9 @@ fun NotesScreen() {
             todoItems.forEachIndexed { index, item ->
                 Row(
                     verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 4.dp)
                 ) {
                     Checkbox(
                         checked = item.isDone,
@@ -111,44 +133,149 @@ fun NotesScreen() {
                                 color = MaterialTheme.colorScheme.onBackground
                             )
                         else
-                            TextStyle(
-                                color = MaterialTheme.colorScheme.onBackground
-                            ),
+                            TextStyle(color = MaterialTheme.colorScheme.onBackground),
                         modifier = Modifier.weight(1f)
                     )
                     IconButton(onClick = { todoItems.removeAt(index) }) {
-                        Icon(Icons.Default.Delete, contentDescription = "Sil",tint = MaterialTheme.colorScheme.onBackground)
+                        Icon(Icons.Default.Delete, contentDescription = "Sil", tint = MaterialTheme.colorScheme.onBackground)
                     }
                 }
             }
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            Button(
-                onClick = {
-                    user?.let {
-                        val notesData = todoItems.map { note ->
-                            mapOf("text" to note.text, "isDone" to note.isDone)
-                        }
-                        firestore.collection("users").document(it.uid)
-                            .update("notes", notesData)
-                            .addOnSuccessListener {
-                                message = "Notlar kaydedildi."
-                            }
-                            .addOnFailureListener { e ->
-                                message = "Hata: ${e.message}"
-                            }
-                    }
-                },
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text("Kaydet")
-            }
-
             if (message.isNotBlank()) {
-                Spacer(modifier = Modifier.height(8.dp))
                 Text(text = message, color = MaterialTheme.colorScheme.primary)
             }
         }
     }
 }
+
+//@Composable
+//fun NotesScreen() {
+//    var todoText by remember { mutableStateOf("") }
+//    val todoItems = remember { mutableStateListOf<TodoItem>() }
+//    val user = FirebaseAuth.getInstance().currentUser
+//    val firestore = FirebaseFirestore.getInstance()
+//    var message by remember { mutableStateOf("") }
+//
+//    // Notları yükle
+//    LaunchedEffect(Unit) {
+//        user?.let {
+//            firestore.collection("users").document(it.uid).get()
+//                .addOnSuccessListener { doc ->
+//                    val notes = doc.get("notes") as? List<Map<String, Any>>
+//                    notes?.forEach {
+//                        val text = it["text"] as? String ?: ""
+//                        val isDone = it["isDone"] as? Boolean ?: false
+//                        todoItems.add(TodoItem(text, isDone))
+//                    }
+//                }
+//        }
+//    }
+//
+//    val themeViewModel: ThemeViewModel = viewModel(
+//        factory = ViewModelProvider.AndroidViewModelFactory(
+//            LocalContext.current.applicationContext as Application
+//        )
+//    )
+//    val isDarkTheme by themeViewModel.isDarkTheme.collectAsState()
+//
+//
+//    // Tüm ekran temaya göre sarılıyor
+//    FitnessAppTheme(darkTheme = isDarkTheme) {
+//
+//        val backgroundColor = if (isDarkTheme) Color.Black else Color.White
+//
+//        Column(
+//            modifier = Modifier
+//                .fillMaxSize()
+//                .background(backgroundColor)
+//                .padding(16.dp),
+//            horizontalAlignment = Alignment.CenterHorizontally
+//        ) {
+//            Spacer(modifier = Modifier.height(32.dp))
+//            Row(verticalAlignment = Alignment.CenterVertically) {
+//                OutlinedTextField(
+//                    value = todoText,
+//                    onValueChange = { todoText = it },
+//                    label = { Text("Yapılacaklar") },
+//                    modifier = Modifier.weight(1f),
+//                    keyboardOptions = KeyboardOptions.Default.copy(
+//                        imeAction = ImeAction.Done,
+//                        keyboardType = KeyboardType.Text
+//                    )
+//                )
+//                Spacer(modifier = Modifier.width(8.dp))
+//                Button(onClick = {
+//                    if (todoText.isNotBlank()) {
+//                        todoItems.add(TodoItem(todoText, false))
+//                        todoText = ""
+//                    }
+//                }) {
+//                    Text("Ekle")
+//                }
+//            }
+//
+//            Spacer(modifier = Modifier.height(16.dp))
+//
+//            todoItems.forEachIndexed { index, item ->
+//                Row(
+//                    verticalAlignment = Alignment.CenterVertically,
+//                    modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
+//                ) {
+//                    Checkbox(
+//                        checked = item.isDone,
+//                        onCheckedChange = {
+//                            todoItems[index] = item.copy(isDone = it)
+//                        }
+//                    )
+//                    Text(
+//                        text = item.text,
+//                        style = if (item.isDone)
+//                            TextStyle(
+//                                textDecoration = TextDecoration.LineThrough,
+//                                color = MaterialTheme.colorScheme.onBackground
+//                            )
+//                        else
+//                            TextStyle(
+//                                color = MaterialTheme.colorScheme.onBackground
+//                            ),
+//                        modifier = Modifier.weight(1f)
+//                    )
+//                    IconButton(onClick = { todoItems.removeAt(index) }) {
+//                        Icon(Icons.Default.Delete, contentDescription = "Sil",tint = MaterialTheme.colorScheme.onBackground)
+//                    }
+//                }
+//            }
+//
+//            Spacer(modifier = Modifier.height(16.dp))
+//
+//            Button(
+//                onClick = {
+//                    user?.let {
+//                        val notesData = todoItems.map { note ->
+//                            mapOf("text" to note.text, "isDone" to note.isDone)
+//                        }
+//                        firestore.collection("users").document(it.uid)
+//                            .update("notes", notesData)
+//                            .addOnSuccessListener {
+//                                message = "Notlar kaydedildi."
+//                            }
+//                            .addOnFailureListener { e ->
+//                                message = "Hata: ${e.message}"
+//                            }
+//                    }
+//                },
+//                modifier = Modifier.fillMaxWidth()
+//            ) {
+//                Text("Kaydet")
+//            }
+//
+//            if (message.isNotBlank()) {
+//                Spacer(modifier = Modifier.height(8.dp))
+//                Text(text = message, color = MaterialTheme.colorScheme.primary)
+//            }
+//        }
+//    }
+//}
